b855a9d59eb5224f208a2edd2116bd9d3277e592
Martin9731 <m.rost.91@gmail.com>
2025-04-02 13:29:36 -0300

    Ejercicio Punto unidad 2

---
+++ Punto/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+    Punto DistanciaEjeCoordenadas = new Punto();
+    Console.WriteLine("Inserte el valor de la coordenada X: ");
+    string CoordX = Console.ReadLine();
+    int CoordenadaX = Convert.ToInt32(CoordX);
+    DistanciaEjeCoordenadas.CoordenadaX = CoordenadaX;
+
+    Console.WriteLine("Inserte el valor de la coordenada Y ");
+    string CoordY = Console.ReadLine();
+    int CoordenadaY = Convert.ToInt32(CoordY);
+    DistanciaEjeCoordenadas.CoordenadaY = CoordenadaY;
+
+    string CoordenadaPunto = DistanciaEjeCoordenadas.CoordenadaPunto();
+
+    Console.WriteLine("Las coordenadas ingresadas del punto son: " + CoordenadaPunto);
+
+    double DistanciaCoordenadas = DistanciaEjeCoordenadas.DistanciaCoordenadas();
+    Console.WriteLine("La distancia del punto al eje de coordenadas es: " + DistanciaCoordenadas);
+
+    Console.WriteLine("Presione cualquier tecla para salir del programa.");
+    Console.ReadKey();
+}
---
+++ Punto/Punto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto
+{
+    internal class Punto
+    {
+        public int CoordenadaX { get; set; }
+
+        public int CoordenadaY { get; set; }
+
+        public string CoordenadaPunto()
+        {
+            string CoordenadaPunto = "";
+            CoordenadaPunto = CoordenadaX.ToString() + ";" + CoordenadaY.ToString();
+            return "(" + CoordenadaPunto + ")";
+        }
+        public double DistanciaCoordenadas()
+        {
+            double DistanciaCoordenadas = 0;
+
+            DistanciaCoordenadas = Math.Sqrt(Math.Pow(CoordenadaX, 2) + Math.Pow(CoordenadaY, 2));
+            return DistanciaCoordenadas;
+
+        }
+
+    }
+}
+
