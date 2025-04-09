234a6da321dc800ce059989598c13cf6d89eb995
MartinR <71902871+Martin9731@users.noreply.github.com>
2025-04-09 16:03:27 -0300

    Add files via upload

---
+++ Biblioteca/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+
+    // pregunta: ¿importa si no nombro las properties igual que Botta? No realmente, es una convención/buena práctica (es mejor hacerlo).
+    // importa si no inicio atributos y solo uso properties? No cambia nada peeeeeeeeero debería usarlos porque es una buena práctica. Los atributos deben ser privados - es fidelidad a la programación orientada a objetos.
+    // importa si no le pongo get y set como hace Botta? el 99% de las veces va a dar igual que esté o no vacío.
+    // Más adelante Oscar va a empezar a usarlo vacío.
+    internal class Libro
+    {
+        private int isbn;
+        private string título;
+        private string autor;
+        private int páginas;
+
+        public int ISBN { get; set; }
+
+        public string Título { get; set; }
+
+        public string Autor { get; set; }
+
+        public int Páginas { get; set; }
+
+        public string MostrarLibro()
+        {
+            return "El libro que usted busca es " + Título + " del autor " + Autor + " dispone de " + Páginas.ToString() + " páginas y su ISBN es " + ISBN.ToString(); 
+        }
+    }
+}
---
+++ Biblioteca/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Program
+
+        //144656532
+
+        // Constructor: forma segura de inicializar todas las propiedades de una clase.
+
+        //dadasjlfkjasdk01312938039
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro();
+            Console.WriteLine("Ingrese el ISBN del libro: ");
+            libro1.ISBN = Convert.ToInt32(Console.ReadLine());
+            // Validar que el valor del ISBN sea igual a 13 números.
+
+            Console.WriteLine("Ingrese el nombre del autor: ");
+            libro1.Autor = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el título del libro ");
+            libro1.Título = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el número de páginas del libro");
+            libro1.Páginas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(libro1.MostrarLibro());
+
+            Console.WriteLine("Presione cualquier tecla para salir del programa. ¡Gracias! ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
