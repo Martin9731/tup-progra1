c762d978ad1ea3ff30005ea77e7bdc54136865c0
MartinR <71902871+Martin9731@users.noreply.github.com>
2025-04-09 16:04:15 -0300

    Add files via upload

---
+++ Verstappen/Oldsmobile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verstappen
+{
+    internal class Auto
+    {
+        //kmarecorrer para el método CONDUCIR, para poder ingresar cuánto precisamente sería lo que se haría.
+        //La regla de negocio establece que cada litro son 11 kilómetros.
+        private double kmarecorrer;
+
+        //el combustible de entrada con el que contamos para el método CONDUCIR para saber si llegamos o no llegamos a nuestro destino.
+        private double combustibleentanque;
+
+        //esta es la capacidad máxima de combustible que tiene el auto para cargar; 49 de tanque, 5 de reserva todo junto porque no se discrimina, ni se distingue.
+        const double capacidadmaxima = 54;
+
+        public double pCombustibleentanque
+        {
+            get { return combustibleentanque; }
+            set
+            {
+                combustibleentanque = Math.Round(value, 2);
+                kmarecorrer = value * 11;
+            }
+        }
+
+        public bool Conducir(double recorridodeusuario)
+
+        {
+            //Console.WriteLine("Por favor, ingrese la cantidad de kilómetros que desea recorrer: ");
+
+
+            if (kmarecorrer >= recorridodeusuario)
+            {
+                recorridodeusuario = kmarecorrer - recorridodeusuario;
+                pCombustibleentanque = recorridodeusuario / 11;
+              //  Console.WriteLine("Puede hacer el recorrido.");
+                return true;
+            }
+            else
+            {
+                //Console.WriteLine("No puede hacer el recorrido con esa cantidad de litros.");
+                return false;
+            }
+
+        }
+        
+        public double CargarCombustible(double litrosacargar)
+        {
+            //Console.WriteLine("Ingrese la cantidad de combustible a cargar en litros: ");
+            
+
+            double combustiblerestante = 0;
+            double combustiblederramado = 0;
+            combustiblerestante = capacidadmaxima - combustibleentanque;
+            
+            if (litrosacargar <= combustiblerestante)
+            {
+                pCombustibleentanque = pCombustibleentanque + litrosacargar;
+                //Console.WriteLine("Se cargaron " + litrosacargar + " y ahora usted dispone de "  + combustibleentanque + " litros.");
+                return 0;
+                
+            }
+            else 
+            {
+                combustiblederramado = litrosacargar - combustiblerestante;
+                //Console.WriteLine("Usted ha cargado todo el tanque, pero ha derramado" + combustiblederramado + " litros.");
+                return combustiblederramado;
+
+            }
+        }
+    }
+}
---
+++ Verstappen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verstappen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Auto Volkswagen = new Auto();
+
+            Console.WriteLine("Por favor, indique la cantidad de nafta con la que arranca el vehículo: ");
+            double litrosacargar = 0;
+            litrosacargar = Convert.ToDouble(Console.ReadLine());
+
+            double resultadocarga;
+            resultadocarga = Volkswagen.CargarCombustible(litrosacargar);
+
+            Console.WriteLine("Por favor, ingrese la cantidad de kilómetros a recorrer: ");
+            double kmarecorrer = 0;
+            kmarecorrer = Convert.ToDouble(Console.ReadLine());
+            bool resultado;
+            resultado = Volkswagen.Conducir(kmarecorrer);
+            if (resultado)
+            {
+                Console.WriteLine("Puede hacer el recorrido.");
+            }
+            else
+            {
+                Console.WriteLine("No puede hacer el recorrido con esa cantidad de litros.");
+            }
+
+            Console.WriteLine("Por favor, indique la cantidad de nafta en litros que desea cargar: ");
+            litrosacargar = 0;
+            litrosacargar = Convert.ToDouble(Console.ReadLine());
+                     
+            resultadocarga = Volkswagen.CargarCombustible(litrosacargar);
+            Console.WriteLine("Se ha derramado una cantidad total de " + resultadocarga + " litros.");
+
+            Console.WriteLine("Presione cualquier tecla para salir.");
+            Console.ReadKey();
+
+        }
+    }
+}
