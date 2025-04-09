c58e15f08990516ab0aa3ae2a7d6326c071e8046
MartinR <71902871+Martin9731@users.noreply.github.com>
2025-04-09 16:02:35 -0300

    Add files via upload

---
+++ PersonaGraft/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaGraft
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Sexo { get; set; }
+
+        public double Altura { get; set; }
+
+        public double Peso { get; set; }
+
+        public double CalcularIMC()
+        {
+            double pesoideal = 0;
+            pesoideal = Peso / Math.Pow(Altura, 2);
+            return pesoideal;
+
+            if (pesoideal < 20)
+            {
+                return -1;
+            }
+            else if (pesoideal >= 20 && pesoideal <= 25)
+            {
+                return 0;
+            }
+            else if (pesoideal > 25)
+            {
+                return 1;
+            }
+        }
+
+        public bool esMayorDeEdad()
+        {
+            if (Edad >= 21)
+            {
+                Console.WriteLine("La persona es mayor a 21 años de edad.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("La persona tiene menos de 21 años de edad.");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ PersonaGraft/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaGraft
+
+ // Recordar que al crear el proyecto inmediatamente hay que crear una clase (en este caso, Persona) a la cual le vamos a asignar atributos, métodos, y propiedades.
+// Luego, llamarla desde acá, o sea el "main"
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona Gabriel = new Persona();
+            Console.WriteLine("Ingrese el nombre de la persona: ");
+            Gabriel.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la edad de la persona en números: ");
+            Gabriel.Edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese H para hombre, o M para mujer. Use solo mayúsculas.");
+            Gabriel.Sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la altura de la persona. Use una coma o un punto para separar metros de centímetros, por ejemplo: 1,80.: ");
+            Gabriel.Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el peso de la persona en kilogramos: ");
+            Gabriel.Peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("El IMC de la persona es " + Gabriel.CalcularIMC());
+
+            Gabriel.esMayorDeEdad();
+
+            Console.WriteLine("Presione cualquier tecla para salir.");
+
+            Console.ReadKey();
+
+        }
+    }
+}
